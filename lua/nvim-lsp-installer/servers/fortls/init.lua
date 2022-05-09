08c9b51e765cb585f7757f6e2c8f7fb038390e00
Ben Weber <dev.ben.weber@gmail.com>
2022-05-09 10:25:21 +0200

    Hacked fortran-language-server to use fortls

--- lua/nvim-lsp-installer/servers/fortls/init.lua
+++ lua/nvim-lsp-installer/servers/fortls/init.lua
@@ -8,7 +8,7 @@ return function(name, root_dir)
         homepage = "https://github.com/hansec/fortran-language-server",
         languages = { "fortran" },
         async = true,
-        installer = pip3.packages { "fortran-language-server" },
+        installer = pip3.packages { "fortls" },
         default_options = {
             cmd_env = pip3.env(root_dir),
         },
